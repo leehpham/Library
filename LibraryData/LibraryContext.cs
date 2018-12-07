d22b848f574769afc0419772d50db61c46994cfe
AYOKA\hoang.pham <hoang.pham@ayokasystems.com>
2018-12-07 11:24:02 -0600

    Initial commit

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
